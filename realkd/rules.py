44dbcb805d3d152c2b45addd6d27e82076a48e65
Mario Boley <mario.boley@gmail.com>
2020-08-26 18:20:40 +1000

    added logistic loss

--- realkd/rules.py
+++ realkd/rules.py
@@ -1,5 +1,5 @@
 from math import inf
-from numpy import arange, array, cumsum, full_like, zeros_like
+from numpy import arange, array, cumsum, exp, full_like, log2, zeros_like
 from pandas import Series
 from realkd.search import Conjunction, Context, KeyValueProposition, Constraint
 
@@ -46,16 +46,72 @@ class SquaredLoss:
         return 'squared'
 
 
+class LogisticLoss:
+    """
+    Logistic loss function l(y, s) = log2(1 + exp(-ys)).
+
+    Function assumes that positive and negative values are encoded as +1 and -1, respectively.
+
+    >>> y = array([1, -1, 1, -1])
+    >>> s = array([0, 0, 10, 10])
+    >>> logistic_loss(y, s)
+    array([1.00000000e+00, 1.00000000e+00, 6.54967668e-05, 1.44270159e+01])
+    >>> logistic_loss.g(y, s)
+    array([-5.00000000e-01,  5.00000000e-01, -4.53978687e-05,  9.99954602e-01])
+    >>> logistic_loss.h(y, s)
+    array([2.50000000e-01, 2.50000000e-01, 4.53958077e-05, 4.53958077e-05])
+    """
+
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(LogisticLoss, cls).__new__(cls)
+        return cls._instance
+
+    @staticmethod
+    def __call__(y, s):
+        return log2(1 + exp(-y*s))
+
+    @staticmethod
+    def sigmoid(a):
+        return 1 / (1 + exp(-a))
+
+    @staticmethod
+    def g(y, s):
+        return -y*LogisticLoss.sigmoid(-y*s)
+
+    @staticmethod
+    def h(y, s):
+        sig = LogisticLoss.sigmoid(-y*s)
+        return sig*(1.0-sig)
+
+    @staticmethod
+    def __repr__():
+        return 'logistic_loss'
+
+    @staticmethod
+    def __str__():
+        return 'logistic'
+
+
+logistic_loss = LogisticLoss()
 squared_loss = SquaredLoss()
 
+
 loss_functions = {
+    LogisticLoss.__repr__(): logistic_loss,
     SquaredLoss.__repr__(): squared_loss,
+    LogisticLoss.__str__(): logistic_loss,
     SquaredLoss.__str__(): squared_loss
 }
 
 
-def loss_function(identifier):
-    return loss_function[identifier]
+def loss_function(loss):
+    if callable(loss):
+        return loss
+    else:
+        return loss_function[loss]
 
 
 class GradientBoostingObjective:
@@ -92,7 +148,7 @@ class GradientBoostingObjective:
         self.data = data
         self.target = target
         self.predictions = predictions or Series(zeros_like(target))
-        self.loss = loss
+        self.loss = loss_function(loss)
         self.reg = reg
         self.g = self.loss.g(self.target, self.predictions)
         self.h = self.loss.h(self.target, self.predictions)
