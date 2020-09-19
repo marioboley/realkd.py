5048ad1684eb75249ba6ff56a8e447c3fe006904
Mario Boley <mario.boley@gmail.com>
2020-09-19 23:37:04 +1000

    removed jupyter sphinx extension dependency for the moment

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,6 @@ release = '0.1.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'jupyter_sphinx',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.mathjax',
