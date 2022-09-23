ddbc3bde1565fe66c5e8489c351a437823ac767d
Alastair-L <lockealastair@gmail.com>
2022-09-23 12:59:10 +1000

    Unskips existing RuleBoostingEstimator doctest

--- realkd/rules.py
+++ realkd/rules.py
@@ -574,7 +574,7 @@ class RuleBoostingEstimator(BaseEstimator):
     >>> survived = titanic.Survived
     >>> titanic.drop(columns=['PassengerId', 'Name', 'Ticket', 'Cabin', 'Survived'], inplace=True)
     >>> re = RuleBoostingEstimator(base_learner=XGBRuleEstimator(loss=logistic_loss))
-    >>> re.fit(titanic, survived.replace(0, -1), verbose=0) # doctest: +SKIP
+    >>> re.fit(titanic, survived.replace(0, -1)).rules_
        -1.4248 if Pclass>=2 & Sex==male
        +1.7471 if Pclass<=2 & Sex==female
        +2.5598 if Age<=19.0 & Fare>=7.8542 & Parch>=1.0 & Sex==male & SibSp<=1.0
