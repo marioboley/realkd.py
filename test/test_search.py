b29dac4607c9a5857c09a54750a7e1c09f6fa178
Alastair Locke <lockealastair@gmail.com>
2023-09-28 17:27:26 +1000

    lint test

--- test/test_search.py
+++ test/test_search.py
@@ -1,34 +1,38 @@
 import unittest
-import realkd.search.core_query_tree
+import realkd.search.exhaustive
 import realkd.legacy.legacy
 
 from doctest import DocTestSuite
 
 
 class SearchContextUdSTestCase(unittest.TestCase):
+    table = [
+        [1, 1, 1, 1, 0],
+        [1, 1, 0, 0, 0],
+        [1, 0, 1, 0, 0],
+        [0, 1, 1, 1, 1],
+        [0, 0, 1, 1, 1],
+        [1, 1, 0, 0, 1],
+    ]
 
-    table = [[1, 1, 1, 1, 0],
-             [1, 1, 0, 0, 0],
-             [1, 0, 1, 0, 0],
-             [0, 1, 1, 1, 1],
-             [0, 0, 1, 1, 1],
-             [1, 1, 0, 0, 1]]
-
-    ctx = realkd.search.core_query_tree.SearchContext.from_tab(table)
+    ctx = realkd.search.exhaustive.SearchContext.from_tab(table)
 
     def test_apx_factor(self):
         labels = [1, 0, 0, 1, 1, 0]
         f = realkd.legacy.legacy.impact(labels)
-        g = realkd.legacy.legacy.cov_incr_mean_bound(labels, realkd.legacy.legacy.impact_count_mean(labels))
-        res = realkd.search.core_query_tree.CoreQueryTreeSearch(self.ctx, f, g, order='bestboundfirst', apx=0.5).run()
-        self.assertEqual(str(res), 'c3')
+        g = realkd.legacy.legacy.cov_incr_mean_bound(
+            labels, realkd.legacy.legacy.impact_count_mean(labels)
+        )
+        res = realkd.search.exhaustive.CoreQueryTreeSearch(
+            self.ctx, f, g, order="bestboundfirst", apx=0.5
+        ).run()
+        self.assertEqual(str(res), "c3")
 
 
 def load_tests(loader, tests, ignore):
-    tests.addTests(DocTestSuite(module=realkd.search.core_query_tree))
+    tests.addTests(DocTestSuite(module=realkd.search.exhaustive))
     return tests
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main(verbosity=2)
-
