7946114a1d06bfea2ac45273fb06af2eb3b34dd4
Alastair Locke <lockealastair@gmail.com>
2023-10-27 14:12:40 +1100

    Keep cleaning

--- test/test_logic.py
+++ test/test_logic.py
@@ -1,14 +1,6 @@
 import unittest
 import realkd.logic
-import sys
-import pathlib
 from doctest import DocTestSuite
-
-
-# print('Running in:', )
-sys.path.append(pathlib.Path(__file__).parent.parent.resolve())
-sys.path.append(pathlib.Path(__file__).parent.resolve())
-
 
 def load_tests(loader, tests, ignore):
     tests.addTests(DocTestSuite(module=realkd.logic))
