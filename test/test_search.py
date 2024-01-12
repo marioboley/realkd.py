983af7485de9d461ce0fbbbabf898d59db720f91
Alastair Locke <lockealastair@gmail.com>
2024-01-12 15:55:51 +1100

    Use 1 test for search

--- test/test_search.py
+++ test/test_search.py
@@ -4,6 +4,7 @@ import realkd.legacy.legacy
 
 from doctest import DocTestSuite
 
+
 @unittest.skip("No tests for legacy code")
 class SearchContextUdSTestCase(unittest.TestCase):
     table = [
@@ -15,7 +16,7 @@ class SearchContextUdSTestCase(unittest.TestCase):
         [1, 1, 0, 0, 1],
     ]
 
-    ctx = realkd.search.exhaustive.SearchContext.from_tab(table)
+    ctx = realkd.search.SearchContext.from_tab(table)
 
     def test_apx_factor(self):
         labels = [1, 0, 0, 1, 1, 0]
@@ -23,14 +24,16 @@ class SearchContextUdSTestCase(unittest.TestCase):
         g = realkd.legacy.legacy.cov_incr_mean_bound(
             labels, realkd.legacy.legacy.impact_count_mean(labels)
         )
-        res = realkd.search.exhaustive.CoreQueryTreeSearch(
+        res = realkd.search.CoreQueryTreeSearch(
             self.ctx, f, g, order="bestboundfirst", apx=0.5
         ).run()
         self.assertEqual(str(res), "c3")
 
 
 def load_tests(loader, tests, ignore):
+    tests.addTests(DocTestSuite(module=realkd.search.context))
     tests.addTests(DocTestSuite(module=realkd.search.exhaustive))
+    tests.addTests(DocTestSuite(module=realkd.search.greedy))
     return tests
 
 
